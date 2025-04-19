91773c78aa384d2f23b3c4be5fa635e4bffd421e
anirudhgv1805 <anirudhgv04@gmail.com>
2025-04-19 16:58:31 +0530

    planned to restructure the project

--- stockease/src/main/java/com/anirudhgv/stockease/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.anirudhgv.stockease.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.anirudhgv.stockease.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long>{
-
-}
