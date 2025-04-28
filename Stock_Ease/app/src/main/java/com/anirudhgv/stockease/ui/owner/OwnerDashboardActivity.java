187e9e384e5c44a922a35d5d493ad92f7a470c40
anirudhgv1805 <anirudhgv04@gmail.com>
2025-04-28 22:21:30 +0530

    updated the owneractivity

--- Stock_Ease/app/src/main/java/com/anirudhgv/stockease/ui/owner/OwnerDashboardActivity.java
+++ Stock_Ease/app/src/main/java/com/anirudhgv/stockease/ui/owner/OwnerDashboardActivity.java
@@ -14,16 +14,10 @@ public class OwnerDashboardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_owner_dashboard);
 
         SessionManager sessionManager = new SessionManager(getApplicationContext());
 
-        MaterialButton logout = findViewById(R.id.logout);
-
-        logout.setOnClickListener(v -> {
-            sessionManager.clearSession();
-        });
 
         Toast.makeText(this, "At OwnerDashboard", Toast.LENGTH_LONG).show();
     }
